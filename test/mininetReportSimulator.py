ebadc1022a9c8fdac74b71f9ca1535fc765a630f
Tri Nguyen <tri.m.nguyen@aalto.fi>
2023-11-21 13:03:26 +0200

    update report simulation

--- test/mininetReportSimulator.py
+++ test/mininetReportSimulator.py
@@ -1,4 +1,6 @@
-import sys, os, argparse
+import sys, os, argparse, json, time
+from qoa4ml.QoaClient import QoaClient
+from qoa4ml import qoaUtils
 MLSOAR_PATH = os.getenv("MLSOAR_PATH")
 sys.path.append(MLSOAR_PATH)
 
@@ -8,23 +10,25 @@ log_path = MLSOAR_PATH + "/log/report/"
 # Main function
 if __name__ == "__main__":
     # Loading default parameter values
-    parser = argparse.ArgumentParser(description="Generate ")
-    parser.add_argument('--log', help='category file', default="/log/")
-    parser.add_argument('--ctr', help='controller IP', default="127.0.0.1")
-    parser.add_argument('--ctrp', help='controller port', default=6633)
-    parser.add_argument('--dc', help='default controller', default="False")
-    parser.add_argument('--df', help='debug flag', default="False")
-    parser.add_argument('--dh', help='debug host', default="127.0.0.1")
-    parser.add_argument('--dp', help='debug port', default=6000)
-    parser.add_argument('--data', help='data folder', default="/data/")
+    parser = argparse.ArgumentParser(description="Generate Mininet Traffic Report")
+    parser.add_argument('--log', help='category file', default="/log/report/")
+    parser.add_argument('--run', help='Experiment Run ID', default=1)
     
     # Parse the parameters
     args = parser.parse_args()
-    log_dir = MLSOAR_PATH+args.log
-    default_controller = bool(args.dc)
-    controller_ip = args.ctr  
-    controller_port = args.ctrp
-    debug_flag = bool(args.df)
-    debug_host = args.dh
-    debug_port = 6000
-    data_path = MLSOAR_PATH+args.data
+    log_file = MLSOAR_PATH+args.log+str(args.run)+"/report.txt"
+    config_file = MLSOAR_PATH+"/configuration/qoaConfig.yaml"
+    qoa_config = qoaUtils.load_config(config_file)
+    qoa_client = QoaClient(config_dict=qoa_config, registration_url=qoa_config["registration_url"])
+    report_data = open(log_file, 'r')
+    data_lines = report_data.readlines()
+    while True:
+        line_count = 0
+        for line in data_lines:
+            jstring = line.replace("'", "\"")
+            line_dict = json.loads(jstring)
+            report = qoa_client.report(report=line_dict,submit=True)
+            print("Sending Report {}".format(line_count))
+            line_count += 1
+            time.sleep(1)
+    
